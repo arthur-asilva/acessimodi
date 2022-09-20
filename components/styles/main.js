42a574f11849860b014fc762f501ae0a943ef3ac
arthur-asilva <dev.arthur.silva@gmail.com>
2022-09-20 19:08:49 -0300

    add base style module

---
+++ components/styles/main.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const primaryColor = '#e1e1e1'
+
+const main = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: primaryColor
+    }
+});
+
+export default main;
